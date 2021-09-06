ffb7ad450e9a088c720b4f7f9166e91b6dff4c2c
Daddy <danyalturikhan@gmail.com>
2021-09-06 23:20:59 +0500

    12 commit

--- chinarconsultants/settings.py
+++ chinarconsultants/settings.py
@@ -53,7 +53,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 ROOT_URLCONF = 'chinarconsultants.urls'
 
 TEMPLATES = [
@@ -129,9 +128,42 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+
+
+
+# Static files (CSS, JavaScript, Images)
+
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+
+STATICFILES_DIRS=(os.path.join(BASE_DIR,'static'),)
+
+
+
 # Default primary key field type
+
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
+
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
